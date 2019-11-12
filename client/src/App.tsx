9568a5aed007d6a8cac98eb4d60a2c9fce58610c
이규종 <kyujong93@naver.com>
2019-11-12 11:29:59 +0900

    [Add] global style과 reset 적용
    global style : 전체 스타일 정의 , app.tsx에서 전역설정
    reset : styled reset의 reset 설정을 적용해서 css 불필요한 설정 제거
    theme : 전역적으로 사용할 스타일 적용. facebook main color, 전체 box style 같은 부분은 여기에 정의

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import client from './apollo/ApolloClient';
+import theme from './style/theme';
+import GlobalStyles from './style/globalStyles';
+import styled, { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from '@apollo/react-hooks';
 
+const Text = styled.span`
+  color: ${props => props.theme.facebookBlue};
+`;
+
 const App: React.FC = () => {
-  return <ApolloProvider client={client}>hello boostbook!</ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Text>hello boostbook!</Text>
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 };
 
 export default App;
