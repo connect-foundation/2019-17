6ea40beb8ba7d9df0c502ef3082c340c5c631a4b
이규종 <kyujong93@naver.com>
2019-11-13 16:10:35 +0900

    [Add] production db 설정 #102
    왜
    production 환경을 develop 과 분리해서 확인하기 위해서
    
    어떻게
    ENV를 PRODUCTION으로 설정할 경우 원격 서버 db와 연결되도록 수정
    후에 env가 더 많아질 경우 cross-env적용해서 환경변수 관리 예정

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,10 +1,16 @@
 import neo4j from "neo4j-driver";
 
+const PRODUCTION = "PRODUCTION";
+const NEO4J_ID: string = process.env.NEO4J_ID || "";
+const NEO4J_PASSWORD: string = process.env.NEO4J_PASSWORD || "";
+const DB_HOST: string = process.env.DB_HOST || "";
+const ENV: string = process.env.ENV || "";
+
 const db = neo4j.driver(
-  "bolt://localhost",
+  `bolt://${ENV === PRODUCTION ? DB_HOST : "localhost"}`,
   neo4j.auth.basic(
-    process.env.NEO4J_ID || "neo4j",
-    process.env.NEO4J_PASSWORD || "neo4j"
+    ENV === PRODUCTION ? NEO4J_ID : "neo4j",
+    ENV === PRODUCTION ? NEO4J_PASSWORD : "neo4j"
   )
 );
 
@@ -13,7 +19,7 @@ db.onCompleted = () => {
 };
 
 db.onError = error => {
-  console.log("😇 db connection fail!");
+  console.log(`😇 db connection fail! ${error}`);
 };
 
 export default db;
