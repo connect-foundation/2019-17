613bb8b9afcae3477ae55c2b82fede93e3d81e18
beingPracticer <lyh00kr2012@gmail.com>
2019-11-14 12:40:25 +0900

    [Add] routing 기능 구현 #82
    client단에서 single page application으로 동작하기에 히스토리기능을 추가하기 위하여 개발하였습니다.
    'react-router-dom'을 이용하여 개발하였으며 constants.ts에서 enum으로 넘겨주는 PAGE_PATHS를 통해 페이지별 path를 설정할 수 있습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import client from './apollo/ApolloClient';
 import theme from './style/theme';
 import GlobalStyles from './style/globalStyles';
-
-const Text = styled.span`
-  color: ${props => props.theme.facebookBlue};
-`;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { PAGE_PATHS } from './constants';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
+import MyPage from './pages/MyPage';
+import NoMatch from './pages/NoMatch'
 
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <Text>hello boostbook!</Text>
+        <Router>
+          <Switch>
+            <Route exact path={PAGE_PATHS.MAIN} component={Main} />
+            <Route path={PAGE_PATHS.SIGNUP} component={SignUp} />
+            <Route path={PAGE_PATHS.SIGNIN} component={SignIn} />
+            <Route path={PAGE_PATHS.MY_PAGE} component={MyPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
       </ThemeProvider>
     </ApolloProvider>
   );
-};
+}
 
 export default App;
---
+++ client/src/constants.ts
@@ -0,0 +1,6 @@
+export enum PAGE_PATHS {
+    SIGNUP = '/signup',
+    SIGNIN = '/signin',
+    MY_PAGE = '/mypage',
+    MAIN = '/'
+}
