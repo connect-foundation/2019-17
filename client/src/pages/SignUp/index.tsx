e3a8fe141f0e54b2fe232ee874f41e60c3ba5b4b
이규종 <kyujong93@naver.com>
2019-11-14 18:33:31 +0900

    merge

--- client/src/pages/SignUp/index.tsx
+++ client/src/pages/SignUp/index.tsx
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const SignUp: React.FC = () => {
-    return (
-        <div>signUp</div>
-    )
-}
-
-export default SignUp;
-=======
 import SignUpContainer from "./SignUpContainer";
 export default SignUpContainer;
->>>>>>> feature-78/profile_page_publishing
