20bfc56744114eeb1e95cfbb84184bc810c871c1
beingPracticer <lyh00kr2012@gmail.com>
2019-11-14 13:28:09 +0900

    [Add] passport를 이용한 google OAuth2.0 추가 #80
    회원가입을 단순화시키기 위해 OAuth를 사용하였습니다.
    path에 auth/google을 추가하여 서버에 접속하면 google 로그인 화면이 나타나게 되며 로그인시 profile이 console에 나타나게 됩니다.

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,17 +3,41 @@ import { GraphQLServer } from 'graphql-yoga';
 import helmet from 'helmet';
 import logger from 'morgan';
 import schema from './schema';
+import dotenv from 'dotenv';
+dotenv.config();
+import passport from 'passport';
+import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
+
 
 class App {
   public app: GraphQLServer;
   constructor() {
     this.app = new GraphQLServer({ schema });
     this.middlewares();
+
+    passport.use(new GoogleStrategy({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:4000/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      //TODO check User and add jwt
+      console.dir(profile);
+    }
+  ));
   }
   private middlewares = (): void => {
     this.app.express.use(cors());
     this.app.express.use(logger('dev'));
     this.app.express.use(helmet());
+    this.app.express.use('/auth/google',
+      passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+    this.app.express.use('/auth/google/callback', 
+      passport.authenticate('google', { failureRedirect: 'http://localhost:3000' }),
+      function(req, res) {
+        //TODO add response with cookie which have jwt
+        res.redirect('http://localhost:3000');
+      });
   };
 }
 
