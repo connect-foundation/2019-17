e7e5a69cab313ce53122fc71ba4f7655d342e390
Yeon. w <vantovan7414@gmail.com>
2019-11-15 17:51:04 +0900

    Add sample test 코드

---
+++ server/test/api/login/sample.test.ts
@@ -0,0 +1,16 @@
+import { requestQuery } from "../../utils";
+import { USER_CHECK } from "./sample.query";
+import { testsScheme } from "./sample.schema";
+
+describe("[테스트 하려는 기능 명세] ex)로그인 성공 확인 ", () => {
+  test("[1개의 상세 테스트에 대한 명세] ex)사용자 정보가 1개 이상 존재하는지 확인", async done => {
+    const body = await requestQuery(USER_CHECK);
+    const result = body.data.sayHello;
+    const { error } = testsScheme.validate(result);
+    if (error) {
+      throw error;
+    }
+
+    done();
+  });
+});
