93938ba7468d172efa0140f4109a4138f651133d
이규종 <kyujong93@naver.com>
2019-11-19 14:04:57 +0900

    [Add] 재사용 가능한 Button 컴포넌트
    textColor, backgroundColor, size(small, medium, big), onChange 옵션을 줄 수 있음
    full -> width 100%

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,21 +1,93 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import theme from '../style/theme';
 
-const SButton = styled.button`
-  all: unset;
+interface ISize {
+  height: string;
+  fontSize: string;
+  width: string;
+}
+
+interface ISizes {
+  [key: string]: ISize;
+}
+
+const sizes: ISizes = {
+  small: {
+    height: '1.5rem',
+    fontSize: '0.75rem',
+    width: '2.5rem'
+  },
+  medium: {
+    height: '1.75rem',
+    fontSize: '0.9rem',
+    width: '3.5rem'
+  },
+  big: {
+    height: '2.25rem',
+    fontSize: '1.25rem',
+    width: '5.5rem'
+  }
+};
+
+const sizeStyles = css`
+  ${({ size }: IProps) =>
+    css`
+      height: ${sizes[size].height};
+      font-size: ${sizes[size].fontSize};
+      width: ${sizes[size].width};
+    `}
+`;
+
+const SButton = styled.button<IProps>`
+  color: ${props => props.textColor};
+  background-color: ${props => props.backgroundColor};
+  border-color: ${props => props.backgroundColor};
+  border-radius: 3px;
+  box-sizing: border-box;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  ${sizeStyles};
 `;
 
 interface IProps {
-  size: string;
   text: string;
+  size: string;
+  backgroundColor: string;
+  textColor: string;
+  full: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLButtonElement>) => void;
 }
 
-function Button({ size, text }: IProps) {
-  return <SButton>{text}</SButton>;
+function Button({
+  size,
+  text,
+  full,
+  textColor,
+  backgroundColor,
+  ...rest
+}: IProps) {
+  return (
+    <SButton
+      text={text}
+      size={size}
+      full={full}
+      textColor={textColor}
+      backgroundColor={backgroundColor}
+      {...rest}
+    >
+      {text}
+    </SButton>
+  );
 }
 
 Button.defaultProps = {
-  size: 'medium'
+  size: 'medium',
+  backgroundColor: theme.colors.facebookBlue,
+  textColor: 'white',
+  full: false
 };
 
 export default Button;
