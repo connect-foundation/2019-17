c67fad7692a307c241745d2a5157bf4bda81e601
beingPracticer <lyh00kr2012@gmail.com>
2019-11-20 22:12:58 +0900

    [Refactor] 팀원들의 review내용 수정
    변수명에 type달기, 불필요한 주석 제거, .gitignore에 존재하는 파일의 캐시제거, 모호한 함수명 변경, destructuring assignment

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,12 +1,23 @@
 import passport from 'passport';
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
 
+const PRODUCTION: string = 'PRODUCTION';
+const NODE_ENV: string = process.env.NODE_ENV || '';
+const LOCAL_SERVER_HOST_ADDRESS: string =
+  process.env.LOCAL_SERVER_HOST_ADDRESS || '';
+const PRODUCTION_SERVER_HOST_ADDRESS: string =
+  process.env.PRODUCTION_SERVER_HOST_ADDRESS || '';
+const SERVER_HOST_ADDRESS: string =
+  PRODUCTION === NODE_ENV
+    ? PRODUCTION_SERVER_HOST_ADDRESS
+    : LOCAL_SERVER_HOST_ADDRESS;
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.SERVER_HOST_ADDRESS + '/auth/google/callback'
+      callbackURL: SERVER_HOST_ADDRESS + '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       done(null, profile);
--- server/src/schema/user/query.ts
+++ server/src/schema/user/query.ts
@@ -1,4 +1,2 @@
 const findUserWithEmailQuery = `MATCH (user:User {email: {email}}) RETURN user`;
-const addUserQuery = `CREATE (user:USER {email: {email}, nickname: {nickname}, hometown: {hometown},
-    residence: {residence}, thumbnail: {thumbnail}}) RETURN user`;
-export { findUserWithEmailQuery, addUserQuery };
+export { findUserWithEmailQuery };
