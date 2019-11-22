30760333555707052e8348ef4f57057dc5739b54
beingPracticer <lyh00kr2012@gmail.com>
2019-11-22 12:02:35 +0900

    [Add] Apollo를 이용한 로컬상태관리 #82
    apollo library를 이용하여 상태관리를 시도하였습니다.
    예제를 적용하여 로그인 상태여부를 쿠키에 존재하는 토큰을 통해 확인하고자 했습니다.
    key값으로만 확인하였습니다.

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+import Pages from '../SignIn';
+import Login from '../SignUp';
+import { resolvers, typeDefs } from './../../resolvers';
 
-const Main: React.FC = () => {
-    return (
-        <div>main</div>
-    )
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql'
+  }),
+  typeDefs,
+  resolvers
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: document.cookie.indexOf('token') !== -1 ? true : false
+  }
+});
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
 }
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+const Main: React.FC = () => {
+  return (
+    <ApolloProvider client={client}>
+      <IsLoggedIn />
+    </ApolloProvider>
+  );
+};
 
 export default Main;
