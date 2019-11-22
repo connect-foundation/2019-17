e328e523122044d47ad1b592af7c4e3b82fb68b2
sang-eun <ashley.ssumu@gmail.com>
2019-11-22 10:43:52 +0900

    [Remove] 리뷰를 위해 임시로 파일 삭제

--- server/test/api/image/image.schema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const imageScheme = Joi.object().keys({
-  filename: Joi.string().required(),
-  mimetype: Joi.string().required(),
-  encoding: Joi.string().required()
-});
