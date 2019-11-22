a36fe7843fe4add7a43913e52999b90e4a709b6c
이규종 <kyujong93@naver.com>
2019-11-22 10:38:31 +0900

    review 요청

---
+++ client/src/__test__/components/Profile.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, cleanup } from '@testing-library/react';
+import Profile from '../../components/Profile';
+
+afterEach(cleanup);
+
+test('<Profile />', () => {
+  const { container, getByAltText, rerender } = render(<Profile />);
+  expect(container).toMatchSnapshot();
+  rerender(<Profile alt={'feed profile'} size={"40px"}/>);
+  expect(getByAltText('feed profile')).toBeInTheDocument();
+});
---
+++ client/src/composition/feed/WritingFeed/WritingFeedContainer.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import WritingFeedPresenter from './WritingPresenter';
+
+function WritingFeedContainer() {
+  const [content, setContent] = useState('');
+  const onChangeTextArea = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    setContent(e.target.value);
+  };
+  return (
+    <WritingFeedPresenter
+      content={content}
+      onChangeTextArea={onChangeTextArea}
+    />
+  );
+}
+
+export default WritingFeedContainer;
