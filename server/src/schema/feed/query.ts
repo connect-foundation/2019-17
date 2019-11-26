49a0026f4700bb4c1637366b2de5117d759b514a
yeon <vantovan7414@gmail.com>
2019-11-27 00:49:38 +0900

    [Add] feed 정보 가지고 오기 api #33 #24
    why
    contents와 createdAt보다 많은 정보가 피드에 필요함
    
    how
    게시글에 딸린 댓글, 작성자, 좋아요 갯수, 좋아요 여부를 포함해서 반환하도록 쿼리 작성

--- server/src/schema/feed/query.ts
+++ server/src/schema/feed/query.ts
@@ -3,4 +3,18 @@ where feed.createdAt <  datetime({cursor})
 RETURN user.nickname as nickname, user.thumbnail as thumbnail ,
 feed.content as content ,apoc.convert.toString(feed.createdAt)  as createdAt 
 LIMIT {first} `;
-export { MATCH_NEW_FEEDS };
+
+const MATCH_FEEDS = `MATCH (searchUser:User)-[:AUTHOR]->(feed:Feed)
+OPTIONAL MATCH (likeUser:User)-[:LIKE]->(feed)
+OPTIONAL MATCH (feed)-[:HAS]->(com:Comment)
+WITH searchUser, feed, COLLECT(likeUser) AS cp , COLLECT(com) as comments
+where feed.createdAt <  datetime({cursor})
+RETURN searchUser , feed , length(cp) AS totallikes,
+length(filter(x IN cp WHERE x.email='dasom@naver.com')) AS hasLiked, comments
+LIMIT {first} `;
+
+const MATCH_NEW_FEEDS2 = `MATCH (user:User { email: 'abc@naver.com' })-[:AUTHOR]->(feed:Feed)
+where feed.createdAt <  datetime('9999-12-31T09:29:26.050Z')
+RETURN feed`;
+
+export { MATCH_NEW_FEEDS, MATCH_NEW_FEEDS2, MATCH_FEEDS };
