b474627c33fbd4eeaca8cd8c3786fc86bf1443b8
sang-eun <ashley.ssumu@gmail.com>
2019-11-27 16:02:46 +0900

    [Add] 유저 이메일 중복체크
    api를 탈취해서 같은 이메일의 유저를 반복 생성할 우려가 있기 때문에
    
    같은 유저 이메일이 존재하는지 체크하고 존재할 경우 에러를 던진다.

--- server/src/api/signUp/signUp.resolvers.ts
+++ server/src/api/signUp/signUp.resolvers.ts
@@ -1,8 +1,19 @@
 import { GraphQLUpload } from 'apollo-upload-server';
 import uploadToObjStorage from '../../middleware/uploadToObjStorage';
-import { SignUpMutationArgs, Token } from '../../types/graph';
+import { SignUpMutationArgs, User } from '../../types/graph';
 import { requestDB } from '../../utils/requestDB';
+import { parseNodeResult } from '../../utils/parseDB';
 import { encodeJWT } from '../../utils/jwt';
+import SameEmailError from '../../errors/EmailAlreadyExistsError';
+
+const checkIsEmailExist = async (email): Promise<void> => {
+  const sameUsers = await requestDB(
+    'Match (u: User { email: $email } ) return u',
+    { email }
+  );
+
+  if (parseNodeResult(sameUsers).length) throw new SameEmailError();
+};
 
 const getFileUrl = async file => {
   const { filename, createReadStream } = await file;
@@ -18,25 +29,27 @@ const getUrlWhenFileExists = args => {
   return null;
 };
 
+const createUser = async info => {
+  const result = await requestDB(
+    `CREATE (u:User {nickname: $nickname, hometown: $hometown, residence: $residence, email: $email ${
+      info.thumbnail ? ', thumbnail: $thumbnail' : ''
+    }
+      }) RETURN u`,
+    info
+  );
+  return result[0].get(0).properties;
+};
+
 export default {
   Mutation: {
-    signUp: async (_, args: SignUpMutationArgs, { res }): Promise<Token> => {
+    signUp: async (_, args: SignUpMutationArgs, { res }): Promise<User> => {
+      await checkIsEmailExist(args.email);
       const thumbnail = await getUrlWhenFileExists(args);
-      const result = await requestDB(
-        `CREATE (a:User {nickname: $nickname, hometown: $hometown, residence: $residence, email: $email ${
-          thumbnail ? ', thumbnail:$thumbnail' : ''
-        }
-          }) RETURN a`,
-        { ...args, thumbnail }
-      );
-
-      const node = result[0].get(0);
-
+      const user = await createUser({ ...args, thumbnail });
       const token: string = await encodeJWT({ email: args.email });
-
       res.cookie('token', token);
 
-      return node.properties;
+      return user;
     }
   },
   Upload: GraphQLUpload
