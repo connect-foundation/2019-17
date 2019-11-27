3fa72e8c83dc623d40805a9637f5cec7a72bd1cd
yeon <vantovan7414@gmail.com>
2019-11-27 15:29:24 +0900

    [Add] jwt 인증 암호화 복호화 파일
    why
    사용자 정보를 알아야함
    how
    쿠키캆을 복호화, 암호화

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import { IKey } from 'src/schema/commonTypes';
+const SECRET: string = process.env.JWT_SECRET || '';
+function encodeJWT(target: IKey<string | number>): string {
+  return jwt.sign(target, SECRET, {
+    expiresIn: '7d',
+    issuer: 'BoostBook',
+    subject: 'authentication'
+  });
+}
+function decodeJWT(token): object | string {
+  return jwt.verify(token, SECRET);
+}
+export { encodeJWT, decodeJWT };
