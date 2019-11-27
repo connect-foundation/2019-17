076580690a8441741d532b695ee8cefb06f91b71
beingPracticer <lyh00kr2012@gmail.com>
2019-11-27 20:50:59 +0900

    [Fix] graphql-codegen과 @client의 충돌로 인한 분리
    graphql-codegen을 사용시 @client에서 오류가 발생하였습니다.
    이를 해결하기 위해서, graphQL 쿼리문에서 client browser cache를 사용한다는 표시인 @client가 들어가는 query를 따로 분리하였습니다.

--- client/src/pages/Routes/LoginRoutes.tsx
+++ client/src/pages/Routes/LoginRoutes.tsx
@@ -3,24 +3,12 @@ import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import AuthRoutes from '../Routes/AuthRoutes';
 import NonAuthRoutes from '../Routes/NonAuthRoutes';
+import { login, getIsLoggedIn } from '../../cache/client.gql';
 
 const LoginRoutes = () => {
-  useQuery(IS_LOGGED_IN);
-  const { data } = useQuery(
-    gql`
-      {
-        isLoggedIn @client
-      }
-    `,
-    { fetchPolicy: 'network-only' }
-  );
+  useQuery(login);
+  const { data } = useQuery(getIsLoggedIn, { fetchPolicy: 'network-only' });
   return data && data.isLoggedIn ? <AuthRoutes /> : <NonAuthRoutes />;
 };
-
-const IS_LOGGED_IN = gql`
-  query IsUserLoggedIn {
-    login @client
-  }
-`;
 
 export default LoginRoutes;
