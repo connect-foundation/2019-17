5d579fbcad6ed97024f28010deafe302c42a3f89
sang-eun <ashley.ssumu@gmail.com>
2019-12-02 16:11:20 +0900

    [Add] 자기 자신은 검색 불가
    자기 자신이 검색 결과로 나와서 친구요청을 하는 상황을 방지하기 위해 쿼리문에서 자신을 제외하도록 수정
    
    쿼리문에서 <> operation을 사용

--- server/src/api/search/searchUser.resolvers.ts
+++ server/src/api/search/searchUser.resolvers.ts
@@ -1,13 +1,17 @@
 import { QuerySearchUserArgs } from '../../types';
 import { requestDB } from '../../utils/requestDB';
-import { findUserWithNicknameQuery } from '../../schema/user/query';
+import { findUserByNicknameQueryWithoutMe } from '../../schema/user/query';
 import { parseNodeResult } from '../../utils/parseDB';
+import { checkReqUserEmail } from '../../utils/context';
 
 export default {
   Query: {
-    searchUser: async (_, { keyword }: QuerySearchUserArgs) => {
-      const result = await requestDB(findUserWithNicknameQuery, {
-        nickname: `${keyword}.*`
+    searchUser: async (_, { keyword }: QuerySearchUserArgs, { req }) => {
+      checkReqUserEmail(req);
+
+      const result = await requestDB(findUserByNicknameQueryWithoutMe, {
+        nickname: `${keyword}.*`,
+        email: req.email
       });
 
       return parseNodeResult(result);
--- server/src/schema/user/query.ts
+++ server/src/schema/user/query.ts
@@ -1,4 +1,9 @@
 const findUserWithEmailQuery = `MATCH (user:User {email: {email}}) RETURN user`;
 const findUserWithNicknameQuery = `MATCH (user:User) WHERE user.nickname=~{nickname} RETURN user`;
+const findUserByNicknameQueryWithoutMe = `MATCH (user:User) WHERE user.nickname=~{nickname} AND user.email <> {email} RETURN user`;
 
-export { findUserWithEmailQuery, findUserWithNicknameQuery };
+export {
+  findUserWithEmailQuery,
+  findUserWithNicknameQuery,
+  findUserByNicknameQueryWithoutMe
+};
