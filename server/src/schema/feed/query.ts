3c1fbe587f08b09cb91bcc06e6f67a3d01aede5d
yeon <vantovan7414@gmail.com>
2019-12-04 18:56:17 +0900

    [Add] 쿼리 export 방식 변경 #140
    컨벤션에 맞춰서 export 방식 변경

--- server/src/schema/feed/query.ts
+++ server/src/schema/feed/query.ts
@@ -1,4 +1,4 @@
-const MATCH_FEEDS = `MATCH (searchUser:User)-[:AUTHOR]->(feed:Feed)
+export const MATCH_FEEDS = `MATCH (searchUser:User)-[:AUTHOR]->(feed:Feed)
 OPTIONAL MATCH (likeUser:User)-[like:LIKE]->(feed)
 OPTIONAL MATCH (feed)-[:HAS]->(com:Comment)
 OPTIONAL MATCH (feed)<-[:HAS]-(img:Image)
@@ -10,18 +10,18 @@ order by feed.createdAt desc
 LIMIT {first} 
 `;
 
-const UPDATE_LIKE = `
+export const UPDATE_LIKE = `
 MATCH (u:User),(f:Feed)
 WHERE u.email = {useremail} AND ID(f) = {feedId}
 MERGE (u)-[r:LIKE]->(f)
 RETURN type(r)`;
 
-const DELETE_LIKE = `
+export const DELETE_LIKE = `
 MATCH (u:User)-[r:LIKE]->(f:Feed)
 WHERE u.email = {useremail} AND ID(f) = {feedId}
 delete r`;
 
-const GET_NEW_FEED = `MATCH (searchUser:User)-[:AUTHOR]->(feed:Feed)
+export const GET_NEW_FEED = `MATCH (searchUser:User)-[:AUTHOR]->(feed:Feed)
 OPTIONAL MATCH (likeUser:User)-[like:LIKE]->(feed)
 OPTIONAL MATCH (feed)-[:HAS]->(com:Comment)
 OPTIONAL MATCH (feed)<-[:HAS]-(img:Image)
@@ -32,9 +32,7 @@ length(filter(x IN cp WHERE x.email= {useremail} )) AS hasLiked, comments
 order by feed.createdAt desc
 `;
 
-const GET_FRIENDS = `MATCH (searchUser:User)-[:FRIEND]->(friend:User)
+export const GET_FRIENDS = `MATCH (searchUser:User)-[:FRIEND]->(friend:User)
 where searchUser.email = {userEmail} and friend.email = {friendEmail}
 with collect(friend) as t
 return length(t) as isFriend`;
-
-export { MATCH_FEEDS, UPDATE_LIKE, DELETE_LIKE, GET_NEW_FEED, GET_FRIENDS };
