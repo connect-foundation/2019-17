ca0d7f920ee3a5f0d8bfcc7181574091f80de7c7
이규종 <kyujong93@naver.com>
2019-12-04 11:04:07 +0900

    [Add] App, WritingFeed test 삭제
    snapshot은 작은 컴포넌트에 한에서 적용.
    큰 컴포넌트는 차후에 E2E 테스트 or 통합테스트 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,39 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-boost';
-import { persistCache } from 'apollo-cache-persist';
-import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import link from './apollo/Link';
-import { resolvers } from './apollo/resolvers';
-import { typeDefs } from './apollo/typeDefs';
+import Apollo from 'apollo/ApolloClient';
 
-const Index: React.FC = () => {
-  const [client, setClient] = useState<ApolloClient<any>>();
-  useEffect(() => {
-    const cache = new InMemoryCache();
-
-    const client = new ApolloClient({
-      cache,
-      link,
-      typeDefs,
-      resolvers
-    });
-
-    persistCache({
-      cache,
-      storage: window.localStorage
-    }).then(() => {
-      setClient(client);
-    });
-  }, []);
-  if (client === undefined) return <div>Loading...</div>;
-  return (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  );
-};
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(
+  <Apollo>
+    <App />
+  </Apollo>,
+  document.getElementById('root')
+);
