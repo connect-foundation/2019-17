87afd32a483011bea60e97daa239dc1127318d84
yeon <vantovan7414@gmail.com>
2019-12-12 18:45:53 +0900

    [Add] useState빠져있어서 추가 #86

--- client/src/composition/Header/AlarmTab/AlarmTabPresenter.tsx
+++ client/src/composition/Header/AlarmTab/AlarmTabPresenter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CommonHeader from '../CommonHeader';
 import CommonFooter from '../CommonFooter';
