e89a476388a724786f0a0551cc6d558f430f67b8
beingPracticer <lyh00kr2012@gmail.com>
2019-12-13 10:54:32 +0900

    [Fix] Object Destructuring

--- client/src/components/FriendProfile.tsx
+++ client/src/components/FriendProfile.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const SIZE = '101px';
+const SIZE = "101px";
 
 const Image = styled.img`
   width: ${SIZE};
@@ -20,19 +20,19 @@ interface IProps {
   email: string;
 }
 
-const FriendProfile: React.FC<IProps> = props => {
+const FriendProfile: React.FC<IProps> = ({ email, thumbnail, nickname }) => {
   return (
     <>
-      <Link to={`/myPage/${props.email}`}>
+      <Link to={`/myPage/${email}`}>
         <Image
           src={
-            props.thumbnail
-              ? props.thumbnail
-              : process.env.PUBLIC_URL + '/images/profile.jpg'
+            thumbnail
+              ? thumbnail
+              : process.env.PUBLIC_URL + "/images/profile.jpg"
           }
-          alt={props.email}
+          alt={email}
         />
-        <Name>{props.nickname}</Name>
+        <Name>{nickname}</Name>
       </Link>
     </>
   );
