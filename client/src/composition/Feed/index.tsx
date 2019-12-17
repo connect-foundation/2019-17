8476d54eedaebe3dcae1d113621fa42477ade11e
yeon <vantovan7414@gmail.com>
2019-12-17 13:33:45 +0900

    [Simpify] 필수로 들어와야 하는 값 필수인지 확인하도록 graphql schema 수정
    피드목록은 무조건 필수이므로 스키마 수정
    해당 스키마에 맞게 Component 수정

--- client/src/composition/Feed/index.tsx
+++ client/src/composition/Feed/index.tsx
@@ -5,7 +5,11 @@ import styled from 'styled-components';
 import WritingFeed from './WritingFeed';
 import NewFeedAlarm from './NewFeedAlarm';
 import NoFeed from './NoFeed';
-import { useGetfeedsQuery, useMeQuery } from 'react-components.d';
+import {
+  useGetfeedsQuery,
+  useMeQuery,
+  GetfeedsQuery
+} from 'react-components.d';
 import { getDate, fullDateFormat } from 'utils/dateUtil';
 import { FEEDS_SUBSCRIPTION } from './feed.query';
 import Loader from 'components/Loader';
@@ -20,6 +24,13 @@ const LoadContainer = styled.div`
   width: 100%;
   height: 10rem;
 `;
+
+const checkCursor = (newCursor, prevCursor) => {
+  if (newCursor === prevCursor) {
+    return '';
+  }
+  return newCursor;
+};
 
 const OFFSET = 4;
 const ALARM_LIMIT = 0;
@@ -60,26 +71,15 @@ const FeedList = () => {
         first: OFFSET,
         currentCursor: data && data.feeds ? data.feeds.cursor : ''
       },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (
-          !fetchMoreResult ||
-          !fetchMoreResult.feeds ||
-          !fetchMoreResult.feeds.feedItems ||
-          !prev.feeds ||
-          !prev.feeds.feedItems ||
-          !fetchMoreResult.feeds.feedItems.length
-        ) {
-          return prev;
-        }
-
+      updateQuery: (
+        prev: GetfeedsQuery,
+        { fetchMoreResult }: { fetchMoreResult: GetfeedsQuery }
+      ) => {
         const {
           feeds: { feedItems, cursor: newCursor }
         } = fetchMoreResult;
-        let finalCursor = newCursor;
 
-        if (newCursor === prev.feeds.cursor) {
-          finalCursor = '';
-        }
+        let finalCursor = checkCursor(newCursor, prev.feeds.cursor);
 
         return Object.assign({}, prev, {
           feeds: {
@@ -101,17 +101,6 @@ const FeedList = () => {
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData.data) return prev;
         const { data: newFeeds } = subscriptionData;
-
-        if (
-          !newFeeds ||
-          !newFeeds.feeds ||
-          !newFeeds.feeds.feedItems ||
-          !prev.feeds ||
-          !prev.feeds.feedItems ||
-          !newFeeds.feeds.feedItems.length
-        ) {
-          return prev;
-        }
 
         const {
           feeds: { feedItems }
