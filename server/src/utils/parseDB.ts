dd82b74aa75928c78cb0658d43d0e9a819cf5615
sang-eun <ashley.ssumu@gmail.com>
2019-12-18 01:18:34 +0900

    [Remove] parseNode 함수 제거
    더 발전된 parseResultRecords를 모두 통일해서 이용하기 위해

--- server/src/utils/parseDB.ts
+++ server/src/utils/parseDB.ts
@@ -1,17 +1,7 @@
-const parseNodeResult = result => {
-  const returnArr: any = [];
-  for (const item of result) {
-    returnArr.push(item.get(0).properties);
-  }
-  return returnArr;
-};
-
-const getNode = result => {
+export const getNode = result => {
   return result[0].get(0).properties;
 };
 
-const getFirstKeyValue = result => {
+export const getFirstKeyValue = result => {
   return result[0].get(0);
 };
-
-export { parseNodeResult, getNode, getFirstKeyValue };
