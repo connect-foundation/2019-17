b96cbcefcdb0cc8f573e401aedcef4dc6a1d7ff6
sang-eun <ashley.ssumu@gmail.com>
2019-12-19 19:17:07 +0900

    [Add] 친구로 등록될 경우 상대방의 실시간 리스트에 자신을 추가
    친구가 될 경우 실시간으로 반영해서 알려줄 필요가 있음
    
    RequestedFrom에서 publish를 해준다.

--- server/src/api/friend/relation/RequestedFrom.ts
+++ server/src/api/friend/relation/RequestedFrom.ts
@@ -3,8 +3,9 @@ import {
   ACCEPT_FRIEND_REQUEST_BY_EMAIL,
   IS_REQUEST_EXIST
 } from '../../../schema/friend/query';
-import { requestDB } from '../../../utils/requestDB';
+import { requestDB, getUserInfoByEmail } from '../../../utils/requestDB';
 import NoRequestError from '../../../errors/NoRequestError';
+import { loginPublish } from '../../user/user.pubsub';
 
 export class RequestedFrom extends Relation {
   getQuery() {
@@ -18,5 +19,11 @@ export class RequestedFrom extends Relation {
     });
 
     if (res[0].get(0) === 'FALSE') throw new NoRequestError();
+  }
+
+  async publish(pubsub, { email }) {
+    const user = await getUserInfoByEmail(email);
+
+    loginPublish({ ...user, status: 'online' });
   }
 }
