be4e8453b09ef1a8012cc688ae8b95ba3b63dc6e
yeon <vantovan7414@gmail.com>
2019-12-19 13:26:04 +0900

    [Add] apollo react test 를 위한 설정 #102
    - apollo react 설정을 위해 mockprovider 모듈 설정
    - mock form을 만들어주는 테스트용 hook 등록

---
+++ client/src/__test__/MockForm.tsx
@@ -0,0 +1,22 @@
+import { MemoryRouter } from 'react-router';
+import { MockedProvider } from '@apollo/react-testing';
+import { ThemeProvider } from 'styled-components';
+import React, { ReactNode } from 'react';
+import theme from 'style/theme';
+
+interface IProps {
+  children?: ReactNode;
+  mocks: any;
+}
+
+const MockForm = ({ mocks, children }: IProps) => {
+  return (
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <MemoryRouter>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </MemoryRouter>
+    </MockedProvider>
+  );
+};
+
+export default MockForm;
